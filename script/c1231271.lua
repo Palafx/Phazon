a15cdbfce433928c1f32b14b10099136981f6a41
Palafx <115953523+Palafx@users.noreply.github.com>
2023-05-21 12:46:51 -0500

    Edited "Shield & Sword"

--- script/c1231271.lua
+++ script/c1231271.lua
@@ -20,18 +20,18 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local tg=Duel.GetAttacker()
 	if chkc then return chkc==tg end
 	if chk==0 then return tg:IsOnField() and tg:IsCanBeEffectTarget(e) end
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	Duel.SetTargetCard(g)
 	Duel.SetTargetCard(tg)
+end
+function s.filter(c,e)
+	return c:IsFaceup() and c:IsRelateToEffect(e) and not c:IsImmuneToEffect(e)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetAttacker()
 	local c=e:GetHandler()
 	if tc:IsRelateToEffect(e) and Duel.NegateAttack() then
 		Duel.BreakEffect()
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_SWAP_BASE_AD)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
 		--Can make a second attack
 		local e2=Effect.CreateEffect(c)
 		e2:SetDescription(3201)
@@ -41,5 +41,14 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetValue(1)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e2)
+		local sg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e)
+		local tcc=sg:GetFirst()
+		for tcc in aux.Next(sg) do
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SWAP_BASE_AD)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tcc:RegisterEffect(e1)
+		end
 	end
 end
