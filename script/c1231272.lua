dec9c8a1b5395869c64f020e59f27bf1a4b484d8
Palafx <115953523+Palafx@users.noreply.github.com>
2023-05-26 09:32:36 -0500

    Fixed cards
    - Dark Sanctuary: Added a delay to the activation effect so it won't miss timing
    - Time Wizard: Its effect to summon Thousand Dragon can now also be used while in the Monster Zone

--- script/c1231272.lua
+++ script/c1231272.lua
@@ -5,7 +5,7 @@ function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_TO_GRAVE)
 	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
