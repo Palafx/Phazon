da3feb472b74b795ee6a02e197e86fc86f9c8aec
Palafx <115953523+Palafx@users.noreply.github.com>
2023-06-18 21:20:26 -0500

    Fixed cards:
    • Darkness 2
    • Darkness 3

--- script/c1231284.lua
+++ script/c1231284.lua
@@ -3,15 +3,25 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(TIMING_DAMAGE_STEP)
 	e1:SetCondition(s.condition)
+	e1:SetCountLimit(1,id)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetOperation(s.activate1)
 	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetOperation(s.activate2)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetOperation(s.activate3)
+	c:RegisterEffect(e3)
 end
 s.listed_series={0x74}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
@@ -26,7 +36,29 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.activate1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(1000)
+		tc:RegisterEffect(e1)
+	end
+end
+function s.activate2(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(2000)
+		tc:RegisterEffect(e1)
+	end
+end
+function s.activate3(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		local e1=Effect.CreateEffect(e:GetHandler())
--- script/c1231285.lua
+++ script/c1231285.lua
@@ -7,18 +7,51 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
 	e1:SetHintTiming(0,TIMING_END_PHASE)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.activate1)
 	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.activate2)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetTarget(s.target3)
+	e3:SetOperation(s.activate3)
+	c:RegisterEffect(e3)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(1000)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1000)
+end
+function s.activate1(e,tp,eg,ep,ev,re,r,rp)
+	local p,d1=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d1,REASON_EFFECT)
+end
+--
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(2000)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,2000)
+end
+function s.activate2(e,tp,eg,ep,ev,re,r,rp)
+	local p,d2=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d2,REASON_EFFECT)
+end
+--
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(1-tp)
 	Duel.SetTargetParam(3000)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,3000)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Damage(p,d,REASON_EFFECT)
+function s.activate3(e,tp,eg,ep,ev,re,r,rp)
+	local p,d3=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d3,REASON_EFFECT)
 end
