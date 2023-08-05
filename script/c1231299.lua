8172367440f19548500e9e0880755c1b9a1ac232
Palafx <115953523+Palafx@users.noreply.github.com>
2023-08-05 11:03:40 -0500

    Add files via upload

--- script/c1231299.lua
+++ script/c1231299.lua
@@ -6,6 +6,7 @@ function s.initial_effect(c)
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
+	e0:SetOperation(s.opp)
 	c:RegisterEffect(e0)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -17,6 +18,26 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	--destroy replace
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_SZONE)
+	--e2:SetCountLimit(1,id)
+	e2:SetTarget(s.desreptg)
+	e2:SetValue(s.desrepval)
+	e2:SetOperation(s.desrepop)
+	c:RegisterEffect(e2)
+	--avoid battle damage
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetTarget(s.efilter)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return tp~=Duel.GetTurnPlayer()
@@ -41,10 +62,53 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
 		Duel.SendtoGrave(g,REASON_EFFECT)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-		local gtt=Duel.SelectMatchingCard(tp,s.pfilter,tp,LOCATION_DECK,0,1,1,nil)
+		local gtt=Duel.SelectMatchingCard(tp,s.pfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil)
 		if #gtt>0 then
 			Duel.BreakEffect()
 			Duel.MoveToField(gtt:GetFirst(),tp,tp,LOCATION_SZONE,POS_FACEUP,true)
 		end
 	end
+end
+function s.repfilter(c,tp)
+	return c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD)
+		and c:IsReason(REASON_BATTLE+REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
+end
+function s.desfilter(c,e,tp)
+	return c:IsControler(tp) and c:IsMonster() and c:IsDestructable(e)
+		and not c:IsStatus(STATUS_DESTROY_CONFIRMED+STATUS_BATTLE_DESTROYED)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsCode(1231300)
+end
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,0,nil)
+	if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp)
+		and g:IsExists(s.desfilter,1,nil,e,tp) end
+	if Duel.SelectEffectYesNo(tp,e:GetHandler(),96) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
+		local sg=g:FilterSelect(tp,s.desfilter,1,1,nil,e,tp)
+		e:SetLabelObject(sg:GetFirst())
+		Duel.HintSelection(sg)
+		sg:GetFirst():SetStatus(STATUS_DESTROY_CONFIRMED,true)
+		return true
+	else return false end
+end
+function s.desrepval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
+end
+function s.desrepop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	tc:SetStatus(STATUS_DESTROY_CONFIRMED,false)
+	Duel.Destroy(tc,REASON_EFFECT+REASON_REPLACE)
+end
+function s.opp(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local gtt=Duel.SelectMatchingCard(tp,s.pfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil)
+	if #gtt>0 then
+		Duel.BreakEffect()
+		Duel.MoveToField(gtt:GetFirst(),tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+	end
+end
+function s.efilter(e,c)
+	return c:IsCode(10000020)
 end
