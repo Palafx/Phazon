b0f6870635a58f335ab874e298ca229fc94b814c
Enrique Palafox <115953523+Palafx@users.noreply.github.com>
2023-12-31 13:12:32 -0600

    Added Jinzo and Utopia Field Spell scripts

---
+++ script/c1231308.lua
@@ -0,0 +1,75 @@
+--Number 39: Utopia (Field Spell)
+--Scripted by EP Custom Cards
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCondition(function(_,tp) return Duel.GetAttacker():IsControler(1-tp) end)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.activate)
+	c:RegisterEffect(e2)
+	--battle indestructable
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
+	c:RegisterEffect(e3)
+	--disable attack
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(s.condition)
+	e4:SetCost(s.cost)
+	e4:SetOperation(s.operation)
+	c:RegisterEffect(e4,false,REGISTER_FLAG_DETACH_XMAT)
+end
+s.listed_series={SET_NUMBER}
+s.xyz_number=39
+--neg attack
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker() and not e:GetHandler():IsStatus(STATUS_CHAINING)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateAttack()
+end
+--special summon
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0x10db,0x11,3,0,0,RACE_WARRIOR,ATTRIBUTE_DARK) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0x107f,0x11,4,2500,2000,RACE_WARRIOR,ATTRIBUTE_DARK) then
+		c:AddMonsterAttribute(TYPE_XYZ+TYPE_EFFECT)
+		Duel.SpecialSummonStep(c,0,tp,tp,true,false,POS_FACEUP)
+		c:AddMonsterAttributeComplete()
+		Duel.SpecialSummonComplete()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_LEVEL_RANK)
+		e1:SetValue(s.xyzlv)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		c:RegisterEffect(e1)
+		Duel.BreakEffect()
+		c:SetStatus(STATUS_NO_LEVEL,true)
+	end
+end
+function s.xyzlv(e,c,rc)
+	return c:GetLevel()
+end
---
+++ script/c1231309.lua
@@ -0,0 +1,57 @@
+--Jinzo (Field Spell)
+--Scripted by EP Custom Cards
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.activate)
+	c:RegisterEffect(e2)
+	--disable
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_DISABLE)
+	e3:SetRange(LOCATION_MZONE+LOCATION_FZONE)
+	e3:SetTargetRange(0,LOCATION_SZONE)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsTrap))
+	c:RegisterEffect(e3)
+	--disable trap monster
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+	e5:SetRange(LOCATION_MZONE+LOCATION_FZONE)
+	e5:SetTargetRange(0,LOCATION_MZONE)
+	e5:SetTarget(aux.TargetBoolFunction(Card.IsTrap))
+	c:RegisterEffect(e5)
+	aux.DoubleSnareValidity(c,LOCATION_MZONE)
+end
+--special summon
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0x10db,0x11,3,0,0,RACE_WARRIOR,ATTRIBUTE_DARK) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.IsPlayerCanSpecialSummonMonster(tp,id,bc,0x11,6,2400,1500,RACE_MACHINE,ATTRIBUTE_DARK) then
+		c:AddMonsterAttribute(TYPE_EFFECT)
+		Duel.SpecialSummonStep(c,0,tp,tp,true,false,POS_FACEUP)
+		c:AddMonsterAttributeComplete()
+		Duel.SpecialSummonComplete()
+	end
+end
+--neg
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local tl=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
+	if tl==LOCATION_SZONE and re:IsActiveType(TYPE_TRAP) then
+		Duel.NegateEffect(ev)
+	end
+end
